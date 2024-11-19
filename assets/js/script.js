645220310c39fad5c0266b66710b894a92021c95
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-19 12:31:48 +0700

    build hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,3 +74,65 @@ window.addEventListener('scroll', function() {
     header.classList.remove('active')
   }
 })
+
+
+/**
+ * HERO
+ * 
+ * 
+ */
+
+const heroSlider = document.querySelector('[data-hero-slider]')
+const heroSliderItem = document.querySelectorAll('[data-hero-slider-item]')
+const heroSliderPrevBtn =  document.querySelector('[data-prev-btn]')
+const heroSliderNextBtn =  document.querySelector('[data-next-btn]')
+
+let currentSlidePos = 0
+let lastActiveSliderItem = heroSliderItem[0]
+
+const updateSliderPos = function () {
+  lastActiveSliderItem.classList.remove('active')
+  heroSliderItem[currentSlidePos].classList.add('active')
+  lastActiveSliderItem = heroSliderItem[currentSlidePos]
+}
+
+const slideNext = function () {
+  if (currentSlidePos >= heroSliderItem.length - 1) {
+    currentSlidePos = 0
+  } else {
+    currentSlidePos++
+  }
+
+  updateSliderPos()
+}
+
+heroSliderNextBtn.addEventListener('click', slideNext)
+
+const slidePrev = function () {
+ if (currentSlidePos <= 0) {
+  currentSlidePos = heroSliderItem.length -1
+ } else {
+  currentSlidePos--
+ }
+
+ updateSliderPos()
+}
+
+heroSliderPrevBtn.addEventListener('click', slidePrev)
+
+
+let autoSlideInterval;
+
+const autoSlide = function () {
+  autoSlideInterval = setInterval(function() {
+    slideNext()
+  }, 7000)
+}
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseover', function() {
+  clearInterval(autoSlideInterval)
+})
+
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseout', autoSlide)
+
+window.addEventListener('load', autoSlide)
