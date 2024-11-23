b4d6a0fb0cfa71916234b219b734a924dfe89a85
Huynh Minh Hai <huynhminhhai.1555@gmail.com>
2024-11-23 18:14:27 +0700

    build button back to top

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@
  */
 
 const preloader = document.querySelector('[data-preload]')
+const backTopBtn = document.querySelector('[data-back-top-btn]')
 
 window.addEventListener('load', function() {
   preloader.classList.add('loaded')
@@ -69,9 +70,11 @@ const hideHeader = function () {
 window.addEventListener('scroll', function() {
   if (window.scrollY >= 50) {
     header.classList.add('active')
+    backTopBtn.classList.add('active')
     hideHeader()
   } else {
     header.classList.remove('active')
+    backTopBtn.classList.remove('active')
   }
 })
 
